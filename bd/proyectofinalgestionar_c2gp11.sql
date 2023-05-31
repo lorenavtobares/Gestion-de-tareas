7dcf2a6c8462dbe06ddc16972fa3dd206c6cea01
Soria Cristian <93338552+Csoria30@users.noreply.github.com>
2023-05-31 12:36:51 -0300

    Entrega 1
    Se entrega del script de la base de datos.
    Documento de diagrama de entidad de relaciones de la base de datos.

---
+++ bd/proyectofinalgestionar_c2gp11.sql
@@ -0,0 +1,95 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 31-05-2023 a las 17:24:36
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `proyectofinalgestionar_c2gp11`
+--
+CREATE DATABASE IF NOT EXISTS `proyectofinalgestionar_c2gp11` DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish_ci;
+USE `proyectofinalgestionar_c2gp11`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `comentarios`
+--
+
+CREATE TABLE `comentarios` (
+  `idComentarios` int(11) NOT NULL,
+  `comentario` varchar(45) NOT NULL,
+  `fechaAvance` date NOT NULL,
+  `idTarea` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `equipo`
+--
+
+CREATE TABLE `equipo` (
+  `idEquipo` int(11) NOT NULL,
+  `idProyecto` int(11) NOT NULL,
+  `nombre` varchar(45) NOT NULL,
+  `fechaCreacion` date NOT NULL,
+  `estado` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `equipomiembros`
+--
+
+CREATE TABLE `equipomiembros` (
+  `idEquipoMiembros` int(11) NOT NULL,
+  `rol` varchar(45) NOT NULL,
+  `fechaIncorporacion` date NOT NULL,
+  `idEquipo` int(11) NOT NULL,
+  `idMiembro` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `miembro`
+--
+
+CREATE TABLE `miembro` (
+  `idMiembro` int(11) NOT NULL,
+  `dni` int(11) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `apellido` varchar(45) NOT NULL,
+  `nombre` varchar(45) NOT NULL,
+  `estado` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `proyecto`
+--
+
+CREATE TABLE `proyecto` (
+  `idProyecto` int(11) NOT NULL,
+  `nombre` varchar(150) NOT NULL,
+  `descripcion` varchar(45) NOT NULL,
+  `fechaInicio` date NOT NULL,
+  `estado` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+<div class="alert alert-danger" role="alert"><img src="themes/dot.gif" title="" alt="" class="icon ic_s_error"> TypeError: str_replace(): Argument #3 ($subject) must be of type array|string, null given</div></body></html>
