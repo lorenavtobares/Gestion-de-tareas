f08819841f1bbf54340e32c4d8446ccf94617d70
Tobares Lorena <lorenav.tobares@gmail.com>
2023-06-11 19:05:17 -0300

    Actualizacion BD - Tarea

--- bd/proyectofinalgestionar_c2gp11.sql
+++ bd/proyectofinalgestionar_c2gp11.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 10-06-2023 a las 05:53:23
+-- Tiempo de generación: 12-06-2023 a las 00:04:01
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -16,53 +16,10 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
-CREATE DATABASE IF NOT EXISTS `proyectofinalgestionar_c2gp11` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `proyectofinalgestionar_c2gp11`;
 
 --
 -- Base de datos: `proyectofinalgestionar_c2gp11`
 --
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `comentarios`
---
-
-CREATE TABLE `comentarios` (
-  `idComentarios` int(11) NOT NULL,
-  `comentario` varchar(45) NOT NULL,
-  `fechaAvance` date NOT NULL,
-  `idTarea` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
-
---
--- Volcado de datos para la tabla `comentarios`
---
-
-INSERT INTO `comentarios` (`idComentarios`, `comentario`, `fechaAvance`, `idTarea`) VALUES
-(1, 'hace bien tu trabajo Marcos', '2023-06-09', 1);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `equipo`
---
-
-CREATE TABLE `equipo` (
-  `idEquipo` int(11) NOT NULL,
-  `idProyecto` int(11) NOT NULL,
-  `nombre` varchar(45) NOT NULL,
-  `fechaCreacion` date NOT NULL,
-  `estado` tinyint(4) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
-
---
--- Volcado de datos para la tabla `equipo`
---
-
-INSERT INTO `equipo` (`idEquipo`, `idProyecto`, `nombre`, `fechaCreacion`, `estado`) VALUES
-(1, 1, 'equipoTest', '2023-06-09', 1);
 
 -- --------------------------------------------------------
 
@@ -85,46 +42,19 @@ CREATE TABLE `equipomiembros` (
 INSERT INTO `equipomiembros` (`idEquipoMiembros`, `rol`, `fechaIncorporacion`, `idEquipo`, `idMiembro`) VALUES
 (1, 'payaso', '2023-06-09', 1, 1);
 
--- --------------------------------------------------------
-
 --
--- Estructura de tabla para la tabla `miembro`
+-- Índices para tablas volcadas
 --
 
-CREATE TABLE `miembro` (
-  `idMiembro` int(11) NOT NULL,
-  `dni` int(11) NOT NULL,
-  `password` varchar(45) NOT NULL,
-  `apellido` varchar(45) NOT NULL,
-  `nombre` varchar(45) NOT NULL,
-  `estado` tinyint(4) NOT NULL,
-  `rolSistema` varchar(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
-
 --
--- Volcado de datos para la tabla `miembro`
+-- Indices de la tabla `equipomiembros`
 --
+ALTER TABLE `equipomiembros`
+  ADD PRIMARY KEY (`idEquipoMiembros`),
+  ADD KEY `idEquipo` (`idEquipo`),
+  ADD KEY `idMiembro` (`idMiembro`);
+COMMIT;
 
-INSERT INTO `miembro` (`idMiembro`, `dni`, `password`, `apellido`, `nombre`, `estado`, `rolSistema`) VALUES
-(1, 25765432, '1234', 'arjona', 'pepe', 1, 'admin');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `proyecto`
---
-
-CREATE TABLE `proyecto` (
-  `idProyecto` int(11) NOT NULL,
-  `nombre` varchar(150) NOT NULL,
-  `descripcion` varchar(45) NOT NULL,
-  `fechaInicio` date NOT NULL,
-  `estado` tinyint(4) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
-
---
--- Volcado de datos para la tabla `proyecto`
---
-
-INSERT INTO `proyecto` (`idProyecto`, `nombre`, `descripcion`, `fechaInicio`, `estado`) VALUES
-(1, 'proyectoPrueba', 'prueba para verificar ', '2023-06-09', 1);
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
