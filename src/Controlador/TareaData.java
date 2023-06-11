a3b0b635456782e17522f178310f8ea12438c5de
Tobares Lorena <lorenav.tobares@gmail.com>
2023-06-11 16:58:42 -0300

    Correcciones

--- src/Controlador/TareaData.java
+++ src/Controlador/TareaData.java
@@ -9,6 +9,9 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JOptionPane;
 
 public class TareaData {
